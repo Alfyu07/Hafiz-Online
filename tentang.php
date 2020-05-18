c57528a0ba52857ae98df655ea0759147443129d
Zikry Ramadhan <zikry399b@gmail.com>
2020-05-18 12:15:15 +0800

    tambah halaman tentang.php

---
+++ tentang.php
@@ -0,0 +1,51 @@
+<?php 
+	require 'functions.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>HafizQuran</title>
+
+		<!-- Font Style -->
+		<link
+			href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+			rel="stylesheet"
+		/>
+		<link rel="stylesheet" href="./css/style.css
+	" />
+	</head>
+	<body>
+		<header>
+			<!-- Navigasi -->
+			<nav>
+				<h1><a href="index.html">HafizQuran</a></h1>
+				<div class="nav-link">
+					<a href="">Tahfiz</a>
+					<a href="">Murajaah</a>
+					<a href="list-artikel.php">Artikel</a>
+					<a href="">Event</a>
+					<a href="">Tentang</a>
+				</div>
+
+				<div class="sign-in">
+					<a href="tambah.php" class="button">Sign Up</a>
+					<a href="">Login</a>
+				</div>
+				<!-- akhir sign up dan login -->
+			</nav>
+			<!-- akhir navigasi -->
+		</header>
+		<!-- Banner landing page -->
+		<div class="banner-container">
+			<div class="title-container">
+				<h2 class="title">Tentang Kami...</h2>
+				<p> Sebuah website untuk membantu mereka yang ingin menghafal Al-Qur'an </p>
+			</div>
+			<div class="banner-image"></div>
+		</div>
+		</div>
+	</body>
+</html>
+
