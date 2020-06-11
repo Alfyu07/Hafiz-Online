cf40b7af1a2b13d55931c6d715b6f0ef2f94c10b
¨Alfyu07¨ <w.alfa433@gmail.com>
2020-06-11 21:46:22 +0800

    menambah konfirmasi di ubahprofile.php

--- santri/ubahprofile.php
+++ santri/ubahprofile.php
@@ -190,6 +190,16 @@ $user = query("SELECT * FROM santri WHERE id_santri = '$id'");
   <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
   <script src="../js/script.js"></script>
   <script src="https://code.iconify.design/1/1.0.6/iconify.min.js"></script>
+  <script>
+    let form = document.querySelector('form');
+    form.addEventListener('submit', (e) => {
+      if (confirm('apakah anda yakin?')) {
+        alert('Profil berhasil diubah');
+      } else {
+        e.preventDefault();
+      }
+    })
+  </script>
 </body>
 
 </html>
